ea2f1fd89e079c1e116312275b425c36ec996a6b
un4ckn0wl3z <slickerth@gmail.com>
2020-07-11 14:38:50 +0700

    add unit test

--- SharpC2/TeamServer.Tests/ClientTests/Client.cs
+++ SharpC2/TeamServer.Tests/ClientTests/Client.cs
@@ -9,18 +9,47 @@ namespace TeamServer.Tests.ClientTests
     public class Client
     {
         [Fact]
-        public void SuccessfulClientLogin()
+        public void _01_SuccessfulClientLogin()
         {
-            var request = new ClientAuthenticationRequest
-            {
-                Nick = "Anuwat",
-                Password = "anuwat1337"
-            };
+            var request = new ClientAuthenticationRequest { Nick = "Anuwat", Password = "anuwat1337" };
 
             var result = Controllers.ClientController.ClientLogin(request);
 
             Assert.Equal(ClientAuthenticationResult.AuthResult.LoginSucess, result.Result);
             Assert.NotNull(result.Token);
+        }
+
+        [Fact]
+        public void _02_BadPasswordClientLogin()
+        {
+            var request = new ClientAuthenticationRequest { Nick = "Anuwat", Password = "anuwat1336" };
+
+            var result = Controllers.ClientController.ClientLogin(request);
+
+            Assert.Equal(ClientAuthenticationResult.AuthResult.BadPassword, result.Result);
+            Assert.Null(result.Token);
+        }
+
+        [Fact]
+        public void _03_NickInUse()
+        {
+            var request = new ClientAuthenticationRequest { Nick = "Anuwat", Password = "anuwat1337" };
+
+            var result = Controllers.ClientController.ClientLogin(request);
+
+            Assert.Equal(ClientAuthenticationResult.AuthResult.NickInUse, result.Result);
+            Assert.Null(result.Token);
+        }
+
+        [Fact]
+        public void _04_InvalidRequest()
+        {
+            var request = new ClientAuthenticationRequest { Nick = "", Password = "" };
+
+            var result = Controllers.ClientController.ClientLogin(request);
+
+            Assert.Equal(ClientAuthenticationResult.AuthResult.InvalidRequest, result.Result);
+            Assert.Null(result.Token);
         }
     }
 }
