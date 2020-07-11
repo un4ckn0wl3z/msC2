a16c99a516669821d499133454f997adb0f64aaa
un4ckn0wl3z <slickerth@gmail.com>
2020-07-11 10:57:54 +0700

    initial TeamServer and config NSwag process infomation.

---
+++ SharpC2/TeamServer/Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NSwag.Generation.Processors.Security;
+
+namespace TeamServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerDocument(c =>
+            {
+                c.PostProcess = d =>
+                {
+                    d.Info.Version = "v1";
+                    d.Info.Title = "SharpC2 API";
+                    d.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Anuwat Khongchuai (un4ckn0wl3z)",
+                        Url = "https://github.com/un4ckn0wl3z/msC2"
+                    };
+
+                };
+                c.DocumentProcessors.Add(new SecurityDefinitionAppender("Bearer", new NSwag.OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "JWT Authorization header using the Bearer scheme.",
+                    Type = NSwag.OpenApiSecuritySchemeType.ApiKey,
+                    In = NSwag.OpenApiSecurityApiKeyLocation.Header
+                }));
+                c.OperationProcessors.Add(new OperationSecurityScopeProcessor("Bearer"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
